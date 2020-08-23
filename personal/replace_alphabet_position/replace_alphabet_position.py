574d3c24efa65e510eadc5aa07e7dcb85d6cac62
euneestella <eunee.stella@gmail.com>
2020-08-24 00:06:54 +0900

    add comment on code

--- personal/replace_alphabet_position/replace_alphabet_position.py
+++ personal/replace_alphabet_position/replace_alphabet_position.py
@@ -1,11 +1,12 @@
 def alphabet_position(text):
+    # Use ASCII Code to incode alphabet into a digit
     pass
     letter_order = []
     for letter in text:
         if letter.islower():
-            letter_order.append(ord(letter)-96)
+            letter_order.append(ord(letter)-(97-1)) # lowercase starts from 97
         elif letter.isupper():
-            letter_order.append(ord(letter)-64)
+            letter_order.append(ord(letter)-(65-1)) # uppercase starts from 65
         else:
             pass
     for letter in range(len(letter_order)):
