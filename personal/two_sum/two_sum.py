75b6fbb8950c0c352bb6f81a5af3b778d2376135
unknown <eunee.stella@gmail.com>
2020-09-28 11:11:59 +0900

    두번째 for문을 가독성 좋게 수정

--- personal/two_sum/two_sum.py
+++ personal/two_sum/two_sum.py
@@ -2,7 +2,7 @@ nums = [2,7,11,15]
 target = 13
 
 def two_sum(nums):
-    for idx, val in enumerate(nums):
-        for index in range(len(nums)-((idx)+1)):
-            if val + nums[len(nums)-(index+1)] == target :
-                return (idx, len(nums)-(index+1))
+    for i, val in enumerate(nums):
+        for j in range(i+1, len(nums)):
+            if val + nums[j] == target :
+                return (i,j)
