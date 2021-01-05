44b9108948d2b34d9c35055ac02a13604446119d
unknown <eunee.stella@gmail.com>
2021-01-05 21:40:26 +0900

    solved: leetcode_1

---
+++ personal/leetcode/1/1_two-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+        for idx, num in enumerate(nums):
+            if target - num in nums_map:
+                return [nums_map[target - num], idx]
+            nums_map[num] = idx
